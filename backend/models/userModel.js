17104aba4ce8d85f2a09faf6944b703e1aa81ecf
gitari9 <gitariveronica968@gmail.com>
2024-09-28 22:54:30 +0300

    order

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,23 @@
 import moongose from "mongoose"
 
 const userSchema = new moongose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true, unique:true},
-    password:{type:String,required:true},
-    cartData:{type:Object,default:{}}
+    name: {
+        type:String,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true, 
+        unique:true
+    },
+    password: {
+        type:String,
+        required:true
+    },
+    cartData: {
+        type:Object,
+        default:{}
+    }
 },{minimize:false})
 
 const userModel = moongose.models.user || moongose.model("user",userSchema);
